354a37967343f75321cd92bbd28318ac9a2875a3
승준 <mia9458ass@gmail.com>
2024-05-07 17:28:55 +0900

    portfolio

--- script.js
+++ script.js
@@ -59,7 +59,63 @@ document.addEventListener("DOMContentLoaded", function() {
     var skillBars = document.querySelectorAll('.skill-bar');
 
     skillBars.forEach(function(skillBar) {
-        var percent = skillBar.dataset.percent;
-        skillBar.style.width = percent + '%';
+        var percent = parseInt(skillBar.dataset.percent) || 0;
+        var skillBarWidth = 300; // 스킬 바의 전체 너비 (예: 200px)
+        var width = (percent / 100) * skillBarWidth; // 퍼센트 값을 픽셀로 변환
+        skillBar.style.width = width + 'px';
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var skillBars = document.querySelectorAll('.skill-bar');
+
+    skillBars.forEach(function(skillBar) {
+        var percent = skillBar.dataset.percent;
+        skillBar.style.setProperty('--percent', percent / 100);
+    });
+});
+
+function openEmailModal() {
+    document.getElementById('emailModal').style.display = 'block';
+}
+
+function closeEmailModal() {
+    document.getElementById('emailModal').style.display = 'none';
+}
+
+function handleSubmit(event) {
+    event.preventDefault(); // 폼이 제출되는 것을 막습니다.
+
+    // 모달을 닫습니다.
+    closeEmailModal();
+
+    // 폼 데이터를 가져옵니다.
+    var form = document.getElementById('emailForm');
+    var formData = new FormData(form);
+
+    // Google Apps Script로 POST 요청을 보냅니다.
+    fetch(form.action, {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.result === 'success') {
+                // 성공 알림을 표시합니다.
+                alert('이메일을 성공적으로 보냈습니다.');
+            } else {
+                // 오류 알림을 표시합니다.
+                alert('이메일을 보내는 중에 오류가 발생했습니다.');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('이메일을 보내는 중에 오류가 발생했습니다.');
+        });
+}
+
+
+
+
+
+
